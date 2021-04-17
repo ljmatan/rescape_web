5a55925c3fead8b481da6ab9085c538417861f98
user <lj.matan@gmail.com>
2021-04-17 18:21:31 +0200

    latest

--- lib/ui/pages/admin/orders/order_display.dart
+++ lib/ui/pages/admin/orders/order_display.dart
@@ -34,7 +34,9 @@ class AdminOrderDisplay extends StatelessWidget {
                               if (!value['error']) rebuild();
                               return value;
                             }))),
-                  ),
+                  )
+                else
+                  const SizedBox(width: 8),
                 RichText(
                   text: TextSpan(
                     style: const TextStyle(
